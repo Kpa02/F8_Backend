903094b79384c611f857606fcb80738b734feefc
Kpa02 <kienduong2k2@gmail.com>
2023-08-16 22:39:41 +0700

    e nop bai a

---
+++ BaiTapB16/Controller/homeController.js
@@ -0,0 +1,15 @@
+module.exports = {
+  index: (req, res) => {
+    if (req.session.isLogged) {
+      return res.render("home/index");
+    } else {
+      return res.redirect("/dang-nhap");
+    }
+  },
+
+  handleLogout: (req, res) => {
+    req.session.isLogged = false;
+    req.flash("success", "Đăng xuất thành công");
+    return res.redirect("/dang-nhap");
+  },
+};
